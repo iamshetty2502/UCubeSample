59eb40e6aeb75ae7e6d2d4c5ab87164ca9e9f147
Sujeet Shetty <43877265+iamshetty2502@users.noreply.github.com>
2020-07-31 22:44:37 +0530

    Updated Sample App
    Sample App modified

--- app/src/main/java/com/sil/ucubesample/MainActivity.java
+++ app/src/main/java/com/sil/ucubesample/MainActivity.java
@@ -1,27 +1,36 @@
 package com.sil.ucubesample;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sil.ucubesample.dialog.CustomDialog;
+import com.sil.ucubesample.utils.Constants;
 import com.sil.ucubesdk.POJO.UCubeRequest;
 import com.sil.ucubesdk.StatusCallBack;
 import com.sil.ucubesdk.UCubeCallBacks;
 import com.sil.ucubesdk.UCubeManager;
 import com.sil.ucubesdk.payment.TransactionType;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final String LICENSEY_KEY = "";
+    private static final String LICENSEY_KEY = "LICENSEY_KEY";
     UCubeManager uCubeManager;
     UCubeRequest uCubeRequest;
-    Button tranxBtn,statusBtn;
+    Button tranxBtn, statusBtn;
+    TextView statusTv, responseCodeTv, responseMessageTv;
+    String trasactionId;
+    CustomDialog customDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,68 +38,146 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tranxBtn = findViewById(R.id.transaction_button);
         statusBtn = findViewById(R.id.status_button);
+        statusTv = findViewById(R.id.status_code);
+        responseCodeTv = findViewById(R.id.response_code);
+        responseMessageTv = findViewById(R.id.response_message);
+
 
         uCubeManager = UCubeManager.getInstance(this, LICENSEY_KEY);
+        customDialog = new CustomDialog(this);
 
-        uCubeRequest = new UCubeRequest();
-
-        uCubeRequest.setUsername("username");
-        uCubeRequest.setPassword("password");
-        uCubeRequest.setRefCompany("companyname");
-        uCubeRequest.setMid("merchantId");
-        uCubeRequest.setTid("TerminalId");
-        uCubeRequest.setImei("IMEI");
-
-        uCubeRequest.setImsi("IMSI");
-        uCubeRequest.setTxn_amount("amount"); //e.g 100.00, 150.50
-        uCubeRequest.setBt_address("Bluetooth_Mac_Address"); //eg xx:yy:zz:aa
-
-        uCubeRequest.setRequestCode(TransactionType.DEBIT);
-
-        tranxBtn.setOnClickListener(this);
-        statusBtn.setOnClickListener(this);
-        tranxBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        Bundle extras = getIntent().getExtras();
+        String bluetoothAdddress = null;
+        if (extras != null) {
+            bluetoothAdddress = extras.getString(Constants.BLUETOOTH_ADDRESS);
+            uCubeRequest = new UCubeRequest();
+            uCubeRequest.setUsername("USERNAME");
+            uCubeRequest.setPassword("PASSWORD");
+            uCubeRequest.setRefCompany("COMPANY_NAME");
+            uCubeRequest.setMid("MERCHANT_ID");
+            uCubeRequest.setTid("TERMINAL_ID");
+            uCubeRequest.setImei("DEVICE_IMEI");
+            uCubeRequest.setImsi("DEVICE_IMSI");
+            uCubeRequest.setTxn_amount("AMOUNT");
+            uCubeRequest.setBt_address(bluetoothAdddress);
+            uCubeRequest.setRequestCode(TransactionType.INQUIRY);
+            tranxBtn.setOnClickListener(this);
+            statusBtn.setOnClickListener(this);
+        }
 
     }
 
     void connectDevice() {
 
+        showStatusDialog(true);
+        trasactionId = uCubeManager.getTransactionId();
         uCubeRequest.setTransactionId(uCubeManager.getTransactionId()); //you can have your own 12digit integer Id or create one using UCubeManager;
-
         uCubeManager.execute(uCubeRequest, new UCubeCallBacks() {
             @Override
             public void successCallback(JSONObject jsonObject) {
+                hideDialog();
                 Log.d(TAG, "successCallback: " + jsonObject);
+                try {
+                    String status = "Success";
+                    int responseCode = -1;
+                    JSONObject responseMessage = new JSONObject();
+
+                    if (jsonObject.has("Msg")) {
+                        status = jsonObject.getString("Msg");
+                    }
+                    if (jsonObject.has("ResponseCode")) {
+                        responseCode = jsonObject.getInt("ResponseCode");
+                    }
+                    if (jsonObject.has("Response")) {
+                        responseMessage = jsonObject.getJSONObject("Response");
+                    }
+
+                    setMessage(status, responseCode, responseMessage.toString());
+                } catch (JSONException jsonexception) {
+                    jsonexception.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
-            public void progressCallback(String s) {
-                Log.d(TAG, "progressCallback: " + s);
+            public void progressCallback(String message) {
+                Log.d(TAG, "progressCallback: " + message);
+                updateTransactionMessage(message);
             }
 
             @Override
             public void failureCallback(JSONObject jsonObject) {
+                hideDialog();
                 Log.d(TAG, "failureCallback: " + jsonObject);
+                try {
+                    String status = "Success";
+                    int responseCode = -1;
+
+                    if (jsonObject.has("Msg")) {
+                        status = jsonObject.getString("Msg");
+                    }
+                    if (jsonObject.has("ResponseCode")) {
+                        responseCode = jsonObject.getInt("ResponseCode");
+                    }
+                    if (responseCode == 100) {
+                        JSONObject responseMessage = new JSONObject();
+                        if (jsonObject.has("Response")) {
+                            responseMessage = jsonObject.getJSONObject("Response");
+                        }
+                        setMessage(status, responseCode, responseMessage.toString());
+                    } else {
+                        String responseMessage = "";
+                        if (jsonObject.has("Response")) {
+                            responseMessage = jsonObject.getString("Response");
+                        }
+                        setMessage(status, responseCode, responseMessage);
+                    }
+                } catch (JSONException jsonexception) {
+                    jsonexception.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    private void hideDialog() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                showStatusDialog(false);
+            }
+        });
+    }
+
+    private void updateTransactionMessage(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (customDialog != null && customDialog.isShowing()) {
+                    customDialog.setMessage(message);
+                }
             }
         });
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.transaction_button: connectDevice();break;
-            case R.id.status_button: checkStatus();break;
+        switch (view.getId()) {
+            case R.id.transaction_button:
+                connectDevice();
+                break;
+            case R.id.status_button:
+                checkStatus();
+                break;
         }
     }
 
     //Check status is applicable only for TransactionType.DEBIT and TransactionType.WITHDRAWAL
     private void checkStatus() {
-
         uCubeManager.checkStatus(uCubeRequest, new StatusCallBack() {
             @Override
             public void successCallback(JSONObject jsonObject) {
@@ -100,6 +187,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void failureCallback(JSONObject jsonObject) {
                 Log.d(TAG, "checkStatus failureCallback: " + jsonObject);
+            }
+        });
+    }
+
+    private void showStatusDialog(boolean show) {
+        if (customDialog != null && customDialog.isShowing() && !show) {
+            customDialog.dismiss();
+            tranxBtn.setEnabled(true);
+            statusBtn.setEnabled(true);
+        } else {
+            customDialog = new CustomDialog(this);
+            customDialog.show();
+            tranxBtn.setEnabled(false);
+            statusBtn.setEnabled(false);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setMessage(final String status, final int responseCode, final String responseMessage) {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if (statusTv != null && status != null && !status.isEmpty()) {
+                    statusTv.setText(status);
+                }
+                if (responseCodeTv != null) {
+                    responseCodeTv.setText(responseCode + "");
+                }
+                if (responseMessageTv != null && responseMessage != null && !responseMessage.isEmpty()) {
+                    responseMessageTv.setText(responseMessage);
+                }
             }
         });
     }
